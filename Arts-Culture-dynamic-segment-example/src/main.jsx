e806a7bdac1e2fe6b1ccba3c3adfecca91ce21ff
Vallor23 <enosnyabinda2017@gmail.com>
2024-08-28 09:33:43 +0300

    Modified route paths

--- Arts-Culture-dynamic-segment-example/src/main.jsx
+++ Arts-Culture-dynamic-segment-example/src/main.jsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route
+} from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+import Home from './pages/home';
+import Content from './components/Content';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Home />}>
+        <Route path='/' element={<Navbar />}>
+          <Route path='content/:contentId' element={<Content />}/>
+        </Route>
+    </Route>
+  )
+)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
 )
